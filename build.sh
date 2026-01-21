f16adb6d46238a0e8abbd87f7e87bc5689873687
Bhakti Bhumare <yourgithubemail@gmail.com>
2026-01-21 22:21:04 +0530

    Fix manage.py path for Render

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+cd agri_system
+
 python manage.py migrate
+python manage.py collectstatic --noinput
